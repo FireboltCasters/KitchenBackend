9dda7e5f85bedefb6e22b79d5d6ad98934f860df
Nils <nilsbaumgartner1994@gmail.com>
2022-01-13 14:38:30 +0100

    Update

--- Backend/extensions/endpoints/redirect-with-token/index.js
+++ Backend/extensions/endpoints/redirect-with-token/index.js
@@ -1,6 +1,4 @@
 /**
- https://gist.github.com/NilsBaumgartner1994/6e1a764491c736d7f187017c9e1f17cc
-
  Place this file into: extensions/endpoints/redirect-with-token/
  For example: https://<PUBLIC_URL>/api/auth/login/<AUTH_PROVIDER>?redirect=https://<PUBLIC_URL>/api/redirect-with-token?redirect=http://localhost?access_token=
  This will get the access_token and will redirect the user to:
